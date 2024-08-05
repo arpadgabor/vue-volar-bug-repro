172b00e9f71b83c0d064a0e43809d51da2e17d31
Arpad Gabor <hello@arpadgabor.com>
2024-08-05 10:14:08 +0300

    feat: repro

---
+++ app/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+
+import { Accordion } from 'ui/ui/accordion';
+</script>
+
+<template>
+  <header>
+    <div>
+      <Accordion></Accordion>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
